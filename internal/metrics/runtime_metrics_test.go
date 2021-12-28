00e8edb0811e038d813ae730642c269cc18c3684
Stepan Ardalin <5485713+OmAsana@users.noreply.github.com>
2021-12-28 14:00:12 +0300

    Add collect metrics test

--- internal/metrics/runtime_metrics_test.go
+++ internal/metrics/runtime_metrics_test.go
@@ -1,28 +1,21 @@
 package metrics
 
 import (
-	"fmt"
-	"reflect"
-	"runtime"
 	"testing"
 )
 
-func TestCollectRuntimeMetrics(t *testing.T) {
-}
-
-func Test_memStats(t *testing.T) {
-	mStats := new(runtime.MemStats)
-	runtime.ReadMemStats(mStats)
-	val := reflect.ValueOf(mStats).Elem()
-	for i := 0; i < val.NumField(); i++ {
-		switch v := val.Field(i).Interface().(type) {
-		case uint64:
-			fmt.Println(v)
-		}
-		//fmt.Println(val.Type().Field(i).Name)
-	}
-}
-
 func Test_CollectRuntimeMetrics(t *testing.T) {
-	CollectRuntimeMetrics()
+	gauges := CollectRuntimeMetrics()
+	for _, statName := range memoryStats {
+		found := false
+		for _, gauge := range gauges {
+			if gauge.Name == statName {
+				found = true
+				break
+			}
+		}
+		if !found {
+			t.Errorf("metric not found: %s", statName)
+		}
+	}
 }
