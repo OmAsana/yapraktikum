024b8cf8414141bd7ed5496fc1b27b527be2329c
Stepan Ardalin <5485713+OmAsana@users.noreply.github.com>
2021-12-28 13:54:04 +0300

    Add agent tests

--- internal/agent/agent.go
+++ internal/agent/agent.go
@@ -2,17 +2,26 @@ package agent
 
 import (
 	"context"
+	"errors"
 	"fmt"
+	"io"
 	"net/http"
+	"net/url"
 	"time"
 
 	"github.com/OmAsana/yapraktikum/internal/metrics"
 )
 
+var defaultBaseURL *url.URL
+
+func init() {
+	defaultBaseURL, _ = url.Parse("http://127.0.0.1:8080")
+}
+
 type Config struct {
 	PollInterval   time.Duration
 	ReportInterval time.Duration
-	PushEndpoint   string
+	BaseURL        *url.URL
 }
 type Agent struct {
 	registry   *metrics.Registry
@@ -26,8 +35,14 @@ func NewDefaultAgent() *Agent {
 		cfg: Config{
 			PollInterval:   time.Second * 2,
 			ReportInterval: time.Second * 10,
-			PushEndpoint:   "http://127.0.0.1:8080",
+			BaseURL:        defaultBaseURL,
 		}}
+}
+
+func NewAgentWithBaseURL(baseURL *url.URL) *Agent {
+	agent := NewDefaultAgent()
+	agent.cfg.BaseURL = baseURL
+	return agent
 }
 
 func (a *Agent) Server(ctx context.Context) {
@@ -36,42 +51,61 @@ func (a *Agent) Server(ctx context.Context) {
 	for {
 		select {
 		case <-pollTicker.C:
-			fmt.Println("poll")
 			a.registry.Collect()
 		case <-reportTicker.C:
 			a.report()
-			fmt.Println("report")
 		case <-ctx.Done():
 			return
 		}
 	}
 }
 
+func (a *Agent) sendRequest(path string) error {
+	rel := &url.URL{Path: path}
+	u := a.cfg.BaseURL.ResolveReference(rel)
+	req, err := http.NewRequest(http.MethodPost, u.String(), nil)
+	if err != nil {
+		return err
+	}
+
+	req.Header.Set("Content-Type", "text/plain")
+
+	resp, err := a.httpClient.Do(req)
+	if err != nil {
+		return err
+	}
+
+	if resp.StatusCode != http.StatusOK {
+		bodyBytes, err := io.ReadAll(resp.Body)
+		if err != nil {
+			return errors.New(err.Error())
+		}
+		return errors.New(fmt.Sprintf("something went wrong: %s", string(bodyBytes)))
+	}
+
+	defer resp.Body.Close()
+	return nil
+
+}
+func (a *Agent) updateGaugeURL(gauge metrics.Gauge) string {
+	return fmt.Sprintf("/update/gauge/%s/%f", gauge.Name, gauge.Value)
+}
+
+func (a *Agent) updateCounterURL(counter metrics.Counter) string {
+	return fmt.Sprintf("/update/counter/%s/%d", counter.Name, counter.Value)
+}
+
 func (a *Agent) report() {
 	for _, gauge := range a.registry.Gauges {
-		url := fmt.Sprintf("%s/update/gauge/%s/%f", a.cfg.PushEndpoint, gauge.Name, gauge.Value)
-		ret, err := a.httpClient.Post(url, "text/plain", nil)
-		if err != nil {
-			fmt.Println(err)
-		}
-		ret.Body.Close()
+		updateURL := a.updateGaugeURL(gauge)
+		_ = a.sendRequest(updateURL)
 	}
 
 	for _, counter := range a.registry.Counters {
-		url := fmt.Sprintf("%s/update/counter/%s/%d", a.cfg.PushEndpoint, counter.Name, counter.Value)
-		ret, err := a.httpClient.Post(url, "text/plain", nil)
-		if err != nil {
-			fmt.Println(err)
-		}
-		ret.Body.Close()
+		updateURL := a.updateCounterURL(counter)
+		_ = a.sendRequest(updateURL)
 
 	}
 
-	url := fmt.Sprintf("%s/update/counter/%s/%d", a.cfg.PushEndpoint, a.registry.PollCounter.Name, a.registry.PollCounter.Value)
-	ret, err := a.httpClient.Post(url, "text/plain", nil)
-	if err != nil {
-		fmt.Println(err)
-	}
-	ret.Body.Close()
-
+	_ = a.sendRequest(a.updateCounterURL(a.registry.PollCounter))
 }
--- internal/agent/agent_test.go
+++ internal/agent/agent_test.go
@@ -2,10 +2,56 @@ package agent
 
 import (
 	"context"
+	"net/http/httptest"
+	"net/url"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+
+	"github.com/OmAsana/yapraktikum/internal/metrics"
+	"github.com/OmAsana/yapraktikum/internal/server"
 )
 
 func ExampleAgent_Server() {
 	ctx := context.Background()
 	agent := NewDefaultAgent()
 	agent.Server(ctx)
+}
+
+func SetupRepo(t *testing.T) server.MetricsRepository {
+	t.Helper()
+	repo := server.NewRepositoryMock()
+	return repo
+}
+
+func TestNewAgent(t *testing.T) {
+	handler := server.NewMetricsServer(SetupRepo(t))
+	metricServer := httptest.NewServer(handler)
+	baseURL, _ := url.Parse(metricServer.URL)
+	agent := NewAgentWithBaseURL(baseURL)
+	agent.httpClient = metricServer.Client()
+	defer metricServer.Close()
+
+	t.Run("Add gauge", func(t *testing.T) {
+		testGauge := metrics.Gauge{
+			Name:  "Test",
+			Value: 1.2,
+		}
+		err := agent.sendRequest(agent.updateGaugeURL(testGauge))
+		require.NoError(t, err)
+	})
+
+	t.Run("Add counter", func(t *testing.T) {
+		testCounter := metrics.Counter{
+			Name:  "testCounter",
+			Value: 1,
+		}
+		err := agent.sendRequest(agent.updateCounterURL(testCounter))
+		require.NoError(t, err)
+	})
+
+	t.Run("Add wrong metric", func(t *testing.T) {
+		err := agent.sendRequest("some/random/val")
+		require.Error(t, err)
+	})
 }
