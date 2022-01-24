62ac3fb9f536aba295aff50b7a93dbd0f4c01407
Stepan Ardalin <5485713+OmAsana@users.noreply.github.com>
2022-01-24 21:50:56 +0300

    Use default compression types

--- internal/server/middleware.go
+++ internal/server/middleware.go
@@ -6,7 +6,9 @@ import (
 	"github.com/go-chi/chi/v5/middleware"
 )
 
-var compressor = middleware.NewCompressor(5, "text/html", "text/css", "application/json")
+var compressor = middleware.NewCompressor(
+	5,
+)
 
 func compressorHandler(next http.Handler) http.Handler {
 	return compressor.Handler(next)
