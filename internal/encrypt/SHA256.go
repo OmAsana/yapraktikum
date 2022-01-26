4d8ef20645c7841b79bd41f5aea30513d8e232fd
Stepan Ardalin <5485713+OmAsana@users.noreply.github.com>
2022-01-26 23:36:39 +0300

    Remove debug out

--- internal/encrypt/SHA256.go
+++ internal/encrypt/SHA256.go
@@ -6,7 +6,6 @@ import (
 	"crypto/hmac"
 	"crypto/sha256"
 	"encoding/hex"
-	"fmt"
 )
 
 func createSHA256Hash(key string) []byte {
@@ -16,7 +15,6 @@ func createSHA256Hash(key string) []byte {
 }
 
 func EncryptSHA256(msg string, key string) string {
-	fmt.Println(msg)
 	h := hmac.New(sha256.New, []byte(key))
 	h.Write([]byte(msg))
 	return hex.EncodeToString(h.Sum(nil))
